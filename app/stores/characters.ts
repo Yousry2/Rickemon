f3edc94ce41a544b4aaace2b694e72d14a201487
Mohamed Elsaka <mohammed.yousry10@gmail.com>
2025-01-28 14:58:59 +0100

    fix/create-new-store-character-details

--- app/stores/characters.ts
+++ app/stores/characters.ts
@@ -46,63 +46,13 @@ export const useCharacterStore = defineStore('characterStore', {
         this.characters = data.results
         this.info = data.info
       }
-      catch (err) {
+      catch {
         this.error = 'Failed to load Rick and Morty characters.'
       }
       finally {
         this.isLoading = false
       }
     },
-
-    async fetchRickAndMortyCharacterDetails(page: number = 1): Promise<void> {
-        this.isLoading = true
-        this.error = null
-        const config = useRuntimeConfig().public.apiParty.endpoints
-        const url = config.rickAndMorty.url
-        try {
-          const response = await fetch(
-            `${url}/character?page=${page}`,
-          )
-          const data = await response.json()
-          this.characters = data.results
-          this.info = data.info
-        }
-        catch (err) {
-          this.error = 'Failed to load Rick and Morty characters.'
-        }
-        finally {
-          this.isLoading = false
-        }
-      },
-
-      async fetchPokemonCharacterDetails(offset: number = 0): Promise<void> {
-        this.isLoading = true
-        this.error = null
-        const config = useRuntimeConfig().public.apiParty.endpoints
-        const url = config.pokemon.url
-        try {
-          const response = await fetch(
-            `${url}/pokemon?offset=${offset}&limit=20`,
-          )
-          const data = await response.json()
-          this.characters = data.results.map((item: { name: string, url: string }) => ({
-            name: item.name,
-            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-              item.url.split('/').slice(-2, -1)[0]
-            }.png`,
-          }))
-          this.info = {
-            next: data.next,
-            prev: data.previous,
-          }
-        }
-        catch (err) {
-          this.error = 'Failed to load Pokémon characters.'
-        }
-        finally {
-          this.isLoading = false
-        }
-      },
 
     async fetchPokemonCharacters(offset: number = 0): Promise<void> {
       this.isLoading = true
@@ -125,7 +75,7 @@ export const useCharacterStore = defineStore('characterStore', {
           prev: data.previous,
         }
       }
-      catch (err) {
+      catch {
         this.error = 'Failed to load Pokémon characters.'
       }
       finally {
